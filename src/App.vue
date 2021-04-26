4eb88688c39327b9668fc52a6b029eb639e8c133
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-04-26 11:12:43 +0300

    vue header component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+<TheHeader></TheHeader>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TheHeader
   }
 }
 </script>
@@ -21,6 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+ <header class="header">
+ COVID 19 Tracker
+ </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: {
+
+  }
+  
+}
+</script>
+
+<style>
+.header{
+background-color: blue;
+    color: white;
+    padding: 1rem;
+}
+
+</style>
